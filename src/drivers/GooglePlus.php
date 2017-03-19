d0ffa188a5363ca5e3f69ce3b14a978d8febff48
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-03-19 21:41:23 +0000

    Create driver for +Google

---
+++ src/drivers/GooglePlus.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @link https://github.com/yiimaker/yii2-social-share
+ * @copyright Copyright (c) 2017 Yii Maker
+ * @license BSD 3-Clause License
+ */
+
+namespace ymaker\social\share\drivers;
+
+use ymaker\social\share\base\Driver;
+
+/**
+ * Driver for https://plus.google.com
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ */
+class GooglePlus extends Driver
+{
+    /**
+     * @inheritdoc
+     */
+    public function getLink()
+    {
+        $this->_link = 'https://plusone.google.com/_/+1/confirm?hl=en&url={url}';
+
+        return parent::getLink();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function processShareData()
+    {
+        $this->url = static::encodeData($this->url);
+    }
+}
