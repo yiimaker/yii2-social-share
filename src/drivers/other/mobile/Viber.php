052e363529d116da611812a32b8bd4bff697a2ad
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-03-20 15:25:52 +0000

    Create driver for Viber messenger

---
+++ src/drivers/other/mobile/Viber.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @link https://github.com/yiimaker/yii2-social-share
+ * @copyright Copyright (c) 2017 Yii Maker
+ * @license BSD 3-Clause License
+ */
+
+namespace ymaker\social\share\drivers\other\mobile;
+
+use ymaker\social\share\base\Driver;
+
+/**
+ * Driver for Viber messenger
+ * @link https://viber.com
+ *
+ * WARNING: This driver works only in mobile devices
+ * with installed Viber client
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ */
+class Viber extends Driver
+{
+    /**
+     * @inheritdoc
+     */
+    public function getLink()
+    {
+        $this->_link = 'viber://forward?text={url}';
+
+        return parent::getLink();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function processShareData()
+    {
+        $this->url = static::encodeData($this->url);
+    }
+}
