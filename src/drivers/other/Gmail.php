106f4a0b256f7de393d00cc615a2befc726ff01c
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-03-20 15:43:24 +0000

    Create driver for Gmail

---
+++ src/drivers/other/Gmail.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @link https://github.com/yiimaker/yii2-social-share
+ * @copyright Copyright (c) 2017 Yii Maker
+ * @license BSD 3-Clause License
+ */
+
+namespace ymaker\social\share\drivers\other;
+
+use ymaker\social\share\base\Driver;
+
+/**
+ * Driver for https://gmail.google.com
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ */
+class Gmail extends Driver
+{
+    /**
+     * @var string
+     */
+    public $bodyPattern = "{description}\n{url}";
+
+
+    /**
+     * @inheritdoc
+     */
+    public function getLink()
+    {
+        $this->_data['{body}'] = strtr($this->bodyPattern, [
+            '{url}'         => $this->url,
+            '{description}' => $this->description,
+            '{imageUrl}'    => $this->imageUrl
+        ]);
+
+        $this->_link = 'https://mail.google.com/mail/?view=cm&fs=1'
+                    . '&su={title}'
+                    . '&body={body}';
+
+        return parent::getLink();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function processShareData()
+    {
+        $this->title = static::encodeData($this->title);
+    }
+}
